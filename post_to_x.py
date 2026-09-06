ef6dd08f5e4086871670cb5941f084a4bf4e032e
gamble20151210-eng <gamble.2015.1210@gmail.com>
2026-09-07 01:01:18 +0900

    Update post_to_x.py

--- post_to_x.py
+++ post_to_x.py
@@ -1,2 +1,17 @@
+import tweepy
+import os
+
 def post_to_x(text):
-    print("投稿処理がここに入る")
+    api_key = os.getenv("TWITTER_API_KEY")
+    api_secret = os.getenv("TWITTER_API_SECRET")
+    access_token = os.getenv("TWITTER_ACCESS_TOKEN")
+    access_secret = os.getenv("TWITTER_ACCESS_SECRET")
+
+    client = tweepy.Client(
+        consumer_key=api_key,
+        consumer_secret=api_secret,
+        access_token=access_token,
+        access_token_secret=access_secret
+    )
+
+    client.create_tweet(text=text)

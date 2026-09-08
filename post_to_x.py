9b31045dfa5a1f0882e423da1d33631e0fce1c34
gamble20151210-eng <gamble.2015.1210@gmail.com>
2026-09-08 22:52:16 +0900

    Update post_to_x.py

--- post_to_x.py
+++ post_to_x.py
@@ -1,17 +1,12 @@
-import tweepy
 import os
+import tweepy
 
 def post_to_x(text):
-    api_key = os.getenv("TWITTER_API_KEY")
-    api_secret = os.getenv("TWITTER_API_SECRET")
-    access_token = os.getenv("TWITTER_ACCESS_TOKEN")
-    access_secret = os.getenv("TWITTER_ACCESS_SECRET")
-
     client = tweepy.Client(
-        consumer_key=api_key,
-        consumer_secret=api_secret,
-        access_token=access_token,
-        access_token_secret=access_secret
+        consumer_key=os.getenv("API_KEY"),
+        consumer_secret=os.getenv("API_KEY_SECRET"),
+        access_token=os.getenv("ACCESS_TOKEN"),
+        access_token_secret=os.getenv("ACCESS_TOKEN_SECRET")
     )
 
     client.create_tweet(text=text)

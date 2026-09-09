4062ec955e69c79e305314750a657f080b0bbb02
gamble20151210-eng <gamble.2015.1210@gmail.com>
2026-09-09 14:56:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from scrape_pokecanavi import scrape_pokecanavi, build_post_text
+from scrape_pokeca_chart import scrape_pokeca_chart, build_post_text
 from post_to_x import post_to_x
 
 def main():
-    singles, boxes = scrape_pokecanavi()
-    text = build_post_text(singles, boxes)
+    results = scrape_pokeca_chart()
+    text = build_post_text(results)
+    print(text)  # デバッグ用
     post_to_x(text)
 
 if __name__ == "__main__":
